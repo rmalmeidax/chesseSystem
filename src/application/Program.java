13e8074f9769f0ebdb0b46bd0476fca32feffc66
Rafael Maranduba <rmalmeidax@gmail.com>
2024-06-03 18:09:15 -0300

    Class ChessMatch

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
-import boardlayer.Board;
-import boardlayer.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position();
-		Board board = new Board (8,8);
-
+		ChessMatch chessmatch = new ChessMatch();
+		
 	}
 
 }
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,21 @@
+package chess;
+
+import boardlayer.Board;
+import boardlayer.Piece;
+
+public class ChessPiece extends Piece {
+	
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	
+
+}
