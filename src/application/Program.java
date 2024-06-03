ab2ae63b7991214e873b2c897418319d16520930
Rafael Maranduba <rmalmeidax@gmail.com>
2024-06-03 15:03:21 -0300

    First Class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardlayer.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Word");
+		Position pos = new Position();
+		
 
 	}
 
